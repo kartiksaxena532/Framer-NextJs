63d5aa4bfb680f5506dd1626de916413144af472
Kartik Saxena <karrtiksaxena@gmail.com>
2024-03-02 22:14:53 +0530

    new hover card

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ import { MaskContainer } from "@/components/svgg-mask";
 import { HeroParallax } from "@/components/parallax";
 import {products} from "@/constants/constants";
 import { GoogleGeminiEffect } from "@/components/goole-gemini";
+import { EvervaultCard, Icon } from "@/components/hoverCard";
 
 import {
   TextRevealCard,
@@ -248,6 +249,23 @@ export default function Home() {
 
 
 </LampContainer>
+
+<div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+ 
+      <EvervaultCard text="hover" />
+ 
+      <h2 className="dark:text-white text-black mt-4 text-sm font-light">
+        Hover over this card to reveal an awesome effect. Running out of copy
+        here.
+      </h2>
+      <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+        Watch me hover
+      </p>
+    </div>
     
     </div>
 
